1bc01d32b538c2e8a630c7762a8a70308cb4926e
leodarkseid <leodarkseid@gmail.com>
2023-02-28 16:22:35 +0100

    first commit

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -3,7 +3,16 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+contract MyToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("MyToken", "MTK") {
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE){
+        _mint(to, amount);
+    }
 }
--- scripts/TestERC20.ts
+++ scripts/TestERC20.ts
@@ -1,12 +1,25 @@
 import { ethers } from "hardhat";
 
 async function main() {
+    const signers = await ethers.getSigners();
+    const signer = signers[0];
     const MyTokenContractFactory = await ethers.getContractFactory("MyToken");
     const MyTokenContract = await MyTokenContractFactory.deploy();
     const deployTxReceipt = await MyTokenContract.deployTransaction.wait();
     console.log(`Token contract was deployed at address ${MyTokenContract.address} at block ${deployTxReceipt.blockNumber}`)
-}
 
+
+    const contractName = await MyTokenContract.name();
+    const contractSymbol = await MyTokenContract.symbol();
+    let totalSupply = await MyTokenContract.totalSupply();
+    console.log(`The contract name is ${contractName} \nThe contract symbol is ${contractSymbol} \nand total supply is ${totalSupply} decimal units`);
+    const mintTx = await MyTokenContract.mint(signer.address, 10);
+    const mintTxReceipt = await mintTx.wait();
+    totalSupply = await MyTokenContract.totalSupply();
+    console.log(
+        `The mint transaction was completed in the Blcok ${mintTxReceipt.blockNumber} \ntotal supply is ${totalSupply} `
+    )
+}
 
 
 main().catch((err) => {
