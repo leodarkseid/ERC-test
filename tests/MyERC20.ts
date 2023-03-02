28c77dbbece0c0d2e9532245d594e1ed3ec0d38f
leodarkseid <leodarkseid@gmail.com>
2023-03-02 16:35:02 +0100

    update

--- tests/MyERC20.ts
+++ tests/MyERC20.ts
@@ -1,11 +1,14 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat"
 import { MyToken } from "../typechain-types";
 
 describe("Basic test for undertstanding ERC20", async ()=>{
 
-    let myTokenContract:MyToken
+    let myTokenContract:MyToken;
+    let signers: SignerWithAddress[];
     beforeEach(async () => {
+        signers = await ethers.getSigners();
         const myTokenContractFactory = await ethers.getContractFactory("MyToken");
         myTokenContract = await myTokenContractFactory.deploy();
         await myTokenContract.deployTransaction.wait();
@@ -14,6 +17,10 @@ describe("Basic test for undertstanding ERC20", async ()=>{
     it("it should have zero total supply at deployment", async () => {
         const totalSupply = await myTokenContract.totalSupply();
         expect(totalSupply).to.eq(0);
+    })
+
+    it ("Triggers transfer event with the address of the sender when sending transations", async () => {
+        await expect(myTokenContract.transfer(signers[1].address, 10)).to.emit(myTokenContract, "Transfer").withArgs();
     })
 });
 
