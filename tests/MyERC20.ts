93bd153525e52dfec607b74a9310ab7cd43a6d55
leodarkseid <leodarkseid@gmail.com>
2023-03-02 16:10:40 +0100

    update

--- tests/MyERC20.ts
+++ tests/MyERC20.ts
@@ -1,12 +1,21 @@
+import { expect } from "chai";
 import { ethers } from "hardhat"
+import { MyToken } from "../typechain-types";
 
 describe("Basic test for undertstanding ERC20", async ()=>{
+
+    let myTokenContract:MyToken
     beforeEach(async () => {
         const myTokenContractFactory = await ethers.getContractFactory("MyToken");
-        const myTokenContract = await myTokenContractFactory.deploy();
-        const deployTxReceipt = myTokenContract.deployTransaction.wait();
+        myTokenContract = await myTokenContractFactory.deploy();
+        await myTokenContract.deployTransaction.wait();
+    });
+
+    it("it should have zero total supply at deployment", async () => {
+        const totalSupply = await myTokenContract.totalSupply();
+        expect(totalSupply).to.eq(0);
     })
-})
+});
 
 function main(){
 
