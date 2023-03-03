e1f743cae8317ea55282fd36683c12f33a2a8e3b
leodarkseid <leodarkseid@gmail.com>
2023-03-03 15:37:01 +0100

    update

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -3,7 +3,7 @@ pragma solidity >0.7 <0.9;
 
 contract TokenSale {
     uint256 public ratio;
-    constructor(uint256 ratio){
-        ratio = ratio;
+    constructor(uint256 _ratio){
+        ratio = _ratio;
     }
 }
